cdb2bce724c2511dbeb49641d9ad62c25c340783
raigal-r <raquel.raigal@outlook.es>
2023-09-02 23:44:00 +0200

    build

--- packages/nextjs/components/contributor-ui/SubmitForm.tsx
+++ packages/nextjs/components/contributor-ui/SubmitForm.tsx
@@ -1,105 +1,119 @@
 // //"use client";
-// import { useState } from "react";
-// import { currencies } from "../../config/currency";
-// import { storageChains } from "../../config/storage-chain";
+import { useState } from "react";
+//  import {
+//   arbitrum,
+//   avalanche,
+//   bsc,
+//   celo,
+//   fantom,
+//   gnosis,
+//   goerli,
+//   mainnet,
+//   moonbeam,
+//   optimism,
+//   polygon,
+// } from "wagmi/chains";
+import { currencies } from "../../config/currency";
+import { storageChains } from "../../config/storage-chain";
 // //import { ConnectButton } from "@rainbow-me/rainbowkit";
 // import "@rainbow-me/rainbowkit/styles.css";
-// import { RequestNetwork, Types, Utils } from "@requestnetwork/request-client.js";
-// import { Web3SignatureProvider } from "@requestnetwork/web3-signature";
-// import { parseUnits, zeroAddress } from "viem";
-// import { useAccount, useWalletClient } from "wagmi";
-import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
+import { RequestNetwork, Types, Utils } from "@requestnetwork/request-client.js";
+import { Web3SignatureProvider } from "@requestnetwork/web3-signature";
+import { parseUnits, zeroAddress } from "viem";
+import { useAccount, useWalletClient } from "wagmi";
 
-// enum APP_STATUS {
-//   AWAITING_INPUT = "awaiting input",
-//   SUBMITTING = "submitting",
-//   PERSISTING_TO_IPFS = "persisting to ipfs",
-//   PERSISTING_ON_CHAIN = "persisting on-chain",
-//   REQUEST_CONFIRMED = "request confirmed",
-//   ERROR_OCCURRED = "error occurred",
-// }
+//import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
+
+enum APP_STATUS {
+  AWAITING_INPUT = "awaiting input",
+  SUBMITTING = "submitting",
+  PERSISTING_TO_IPFS = "persisting to ipfs",
+  PERSISTING_ON_CHAIN = "persisting on-chain",
+  REQUEST_CONFIRMED = "request confirmed",
+  ERROR_OCCURRED = "error occurred",
+}
 
 export default function SubmitForm() {
-  //   const [storageChain, setStorageChain] = useState("5");
-  //   const [expectedAmount, setExpectedAmount] = useState("");
-  //   const [currency, setCurrency] = useState("5_0xBA62BCfcAaFc6622853cca2BE6Ac7d845BC0f2Dc");
+  const [storageChain /*, setStorageChain*/] = useState("5");
+  const [expectedAmount /*, setExpectedAmount*/] = useState("");
+  const [currency /*, setCurrency*/] = useState("5_0xBA62BCfcAaFc6622853cca2BE6Ac7d845BC0f2Dc");
   //   //const [paymentRecipient, setPaymentRecipient] = useState("");
-  //   const [payerIdentity, setPayerIdentity] = useState("");
-  //   const [dueDate /*setDueDate*/] = useState("");
-  //   const [reason, setReason] = useState("");
-  //   const [status, setStatus] = useState(APP_STATUS.AWAITING_INPUT);
+  const [payerIdentity /*, setPayerIdentity*/] = useState("");
+  const [dueDate /*setDueDate*/] = useState("");
+  const [reason /*, setReason*/] = useState("");
+  const [status, setStatus] = useState(APP_STATUS.AWAITING_INPUT);
 
-  //   const { data: walletClient, isError, isLoading } = useWalletClient();
-  //   const { address, isConnecting, isDisconnected } = useAccount();
-  //   const [requestData, setRequestData] = useState<Types.IRequestDataWithEvents>();
+  const { data: walletClient /*, isError, isLoading */ } = useWalletClient();
+  const { address /*, isConnecting, isDisconnected */ } = useAccount();
+  const [requestData, setRequestData] = useState<Types.IRequestDataWithEvents>();
 
-  //   async function createRequest() {
-  //     const signatureProvider = new Web3SignatureProvider(walletClient);
-  //     const requestClient = new RequestNetwork({
-  //       nodeConnectionConfig: {
-  //         baseURL: storageChains.get(storageChain)!.gateway,
-  //       },
-  //       signatureProvider,
-  //       // httpConfig: {
-  //       //   getConfirmationMaxRetry: 40, // timeout after 120 seconds
-  //       // },
-  //     });
-  //     const requestCreateParameters: Types.ICreateRequestParameters = {
-  //       requestInfo: {
-  //         currency: {
-  //           type: currencies.get(currency)!.type,
-  //           value: currencies.get(currency)!.value,
-  //           network: currencies.get(currency)!.network,
-  //         },
-  //         expectedAmount: parseUnits(expectedAmount as `${number}`, currencies.get(currency)!.decimals).toString(),
-  //         payee: {
-  //           type: Types.Identity.TYPE.ETHEREUM_ADDRESS,
-  //           value: address as string,
-  //         },
-  //         timestamp: Utils.getCurrentTimestampInSecond(),
-  //       },
-  //       paymentNetwork: {
-  //         id: Types.Extension.PAYMENT_NETWORK_ID.ERC20_FEE_PROXY_CONTRACT,
-  //         parameters: {
-  //           paymentNetworkName: currencies.get(currency)!.network,
-  //           paymentAddress: /*paymentRecipient || */ address,
-  //           feeAddress: zeroAddress,
-  //           feeAmount: "0",
-  //         },
-  //       },
-  //       contentData: {
-  //         // Consider using rnf_invoice format from @requestnetwork/data-format package.
-  //         reason: reason,
-  //         dueDate: dueDate,
-  //       },
-  //       signer: {
-  //         type: Types.Identity.TYPE.ETHEREUM_ADDRESS,
-  //         value: address as string,
-  //       },
-  //     };
+  async function createRequest() {
+    const signatureProvider = new Web3SignatureProvider(walletClient);
+    const requestClient = new RequestNetwork({
+      nodeConnectionConfig: {
+        baseURL: storageChains.get(storageChain)!.gateway,
+      },
+      signatureProvider,
+      httpConfig: {
+        getConfirmationMaxRetry: 40, // timeout after 120 seconds
+      },
+    });
+    const requestCreateParameters: Types.ICreateRequestParameters = {
+      requestInfo: {
+        currency: {
+          type: currencies.get(currency)!.type,
+          value: currencies.get(currency)!.value,
+          network: currencies.get(currency)!.network,
+        },
+        expectedAmount: parseUnits(expectedAmount as `${number}`, currencies.get(currency)!.decimals).toString(),
+        payee: {
+          type: Types.Identity.TYPE.ETHEREUM_ADDRESS,
+          value: address as string,
+        },
+        timestamp: Utils.getCurrentTimestampInSecond(),
+      },
+      paymentNetwork: {
+        id: Types.Extension.PAYMENT_NETWORK_ID.ERC20_FEE_PROXY_CONTRACT,
+        parameters: {
+          paymentNetworkName: currencies.get(currency)!.network,
+          paymentAddress: /*paymentRecipient || */ address,
+          feeAddress: zeroAddress,
+          feeAmount: "0",
+        },
+      },
+      contentData: {
+        // Consider using rnf_invoice format from @requestnetwork/data-format package.
+        reason: reason,
+        dueDate: dueDate,
+      },
+      signer: {
+        type: Types.Identity.TYPE.ETHEREUM_ADDRESS,
+        value: address as string,
+      },
+    };
 
-  //     if (payerIdentity.length > 0) {
-  //       requestCreateParameters.requestInfo.payer = {
-  //         type: Types.Identity.TYPE.ETHEREUM_ADDRESS,
-  //         value: payerIdentity,
-  //       };
-  //     }
+    if (payerIdentity.length > 0) {
+      requestCreateParameters.requestInfo.payer = {
+        type: Types.Identity.TYPE.ETHEREUM_ADDRESS,
+        value: payerIdentity,
+      };
+    }
 
-  //     try {
-  //       setStatus(APP_STATUS.PERSISTING_TO_IPFS);
-  //       const request = await requestClient.createRequest(requestCreateParameters);
+    try {
+      setStatus(APP_STATUS.PERSISTING_TO_IPFS);
+      const request = await requestClient.createRequest(requestCreateParameters);
 
-  //       setStatus(APP_STATUS.PERSISTING_ON_CHAIN);
-  //       setRequestData(request.getData());
-  //       const confirmedRequestData = await request.waitForConfirmation();
+      setStatus(APP_STATUS.PERSISTING_ON_CHAIN);
+      setRequestData(request.getData());
+      const confirmedRequestData = await request.waitForConfirmation();
 
-  //       setStatus(APP_STATUS.REQUEST_CONFIRMED);
-  //       setRequestData(confirmedRequestData);
-  //     } catch (err) {
-  //       setStatus(APP_STATUS.ERROR_OCCURRED);
-  //       alert(err);
-  //     }
-  //   }
+      setStatus(APP_STATUS.REQUEST_CONFIRMED);
+      setRequestData(confirmedRequestData);
+    } catch (err) {
+      setStatus(APP_STATUS.ERROR_OCCURRED);
+      alert(err);
+    }
+  }
 
   //   function canSubmit() {
   //     return (
@@ -118,27 +132,24 @@ export default function SubmitForm() {
   //     );
   //   }
 
-  //   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-  //     e.preventDefault();
-  //     if (!canSubmit()) {
-  //       return;
-  //     }
-  //     setRequestData(undefined);
-  //     setStatus(APP_STATUS.SUBMITTING);
-  //     createRequest();
-  //   }
+  function handleSubmit(/*e: React.FormEvent<HTMLFormElement>*/) {
+    //     e.preventDefault();
+    //     if (!canSubmit()) {
+    //       return;
+    //     }
+    //     setRequestData(undefined);
+    //     setStatus(APP_STATUS.SUBMITTING);
+    createRequest();
+  }
 
   //   function handleClear() {
   //     setRequestData(undefined);
   //     setStatus(APP_STATUS.AWAITING_INPUT);
   //   }
 
-  return (
-    <div>
-      <RainbowKitCustomConnectButton />
-      <FaucetButton />
-    </div>
-  );
+  handleSubmit();
+
+  return <div></div>;
   //     <div>
   //       <h3>Create a request</h3>
   //       <br></br>
@@ -253,16 +264,16 @@ export default function SubmitForm() {
   //         </button>
   //       </form>
   //       <br></br>
-  //       <div>
-  //         <h3>Created request</h3>
-  //         <button type="button" onClick={handleClear}>
-  //           Clear
-  //         </button>
-  //         <p>App status: {status}</p>
-  //         <p>Request state: {requestData?.state}</p>
-  //         <p>Request data:</p>
-  //         <pre>{JSON.stringify(requestData, undefined, 2)}</pre>
-  //       </div>
+  <div>
+    <h3>Created request</h3>
+    {/* <button type="button" onClick={handleClear}>
+             Clear
+           </button> */}
+    <p>App status: {status}</p>
+    <p>Request state: {requestData?.state}</p>
+    <p>Request data:</p>
+    <pre>{JSON.stringify(requestData, undefined, 2)}</pre>/{" "}
+  </div>;
   //     </div>
   //   );
 }
