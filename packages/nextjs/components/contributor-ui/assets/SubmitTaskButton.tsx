03c9e9da95e87b13b50385aeaf04c06ce791921f
raigal-r <105627233+raigal-r@users.noreply.github.com>
2023-09-03 03:59:02 +0300

    Update SubmitTaskButton.tsx

--- packages/nextjs/components/contributor-ui/assets/SubmitTaskButton.tsx
+++ packages/nextjs/components/contributor-ui/assets/SubmitTaskButton.tsx
@@ -4,11 +4,14 @@ import Link from "next/link";
 const SubmitTaskButton = () => {
   return (
     <>
+    <div className="flex flex-col border border-primary rounded-2xl p-1 flex-shrink-0 pl-10 pr-10 mt-10 mb-10">
       <button>
-        <Link href="/contributor-form" passHref className="link">
-          Submission Form
-        </Link>{" "}
-      </button>
+          <Link href="/contributor-form" passHref className="link">
+            Submission Form
+          </Link>{" "}
+        </button>
+    </div>
+     
     </>
   );
 };
