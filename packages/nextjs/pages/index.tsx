eb6bb43613fc0891888c0b9483891bb2542aef34
raigal-r <105627233+raigal-r@users.noreply.github.com>
2023-09-03 04:12:50 +0300

    Update index.tsx

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
         <div className="px-5">
           <h1 className="text-center mb-8">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">LEND your TIME</span>
+            <span className="block text-4xl font-bold">PEERFinance</span>
           </h1>
 
           {/* <p className="text-center text-lg">
