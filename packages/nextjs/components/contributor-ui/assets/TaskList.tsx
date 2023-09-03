ab0c41825c725a50899b33873fb8ff71e6506485
raigal-r <105627233+raigal-r@users.noreply.github.com>
2023-09-03 04:01:52 +0300

    Update TaskList.tsx

--- packages/nextjs/components/contributor-ui/assets/TaskList.tsx
+++ packages/nextjs/components/contributor-ui/assets/TaskList.tsx
@@ -1,23 +1,12 @@
 export const TaskList = () => {
   return (
-    <div className="flex justify-center px-4 md:px-0">
+    <div className="flex justify-center item-center px-4 md:px-0">
       <div className="overflow-x-auto w-full shadow-2xl rounded-xl">
         <div>
-          <h1>Approved</h1>
+          <h1 className="text-center">You don't have any approved task</h1>
 
-          <div>
-            <p>NFTs Pending to be claimed</p>
-          </div>
-          <div>
-            <p>NFTs claimed but not executed</p>
-          </div>
-          <div>
-            <p>NFT alredy burned</p>
-          </div>
         </div>
-        <div>
-          <h1>Rejected</h1>
-        </div>
+          
       </div>
     </div>
   );
